c3378bac4139b4c52d996b598b7f1d0297295ad2
Stephen Magill <stephen.magill@gmail.com>
2021-12-17 08:32:48 -0500

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,7 +6,16 @@ namespace hello_world
     {
         public string ReturnMessage()
         {
-            return "Hello World!";
+            using (var sha1 = new SHA1Managed())
+            {
+                byte[] hash = sha1.ComputeHash("Hello World!");
+                StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                foreach (byte b in hash)
+                {
+                    formatted.AppendFormat("{0:X2}", b);
+                }
+                return formatted.ToString();
+            }
         }
     }
 }
